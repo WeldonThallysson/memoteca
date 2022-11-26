b3a38bc7e877e8e9cab90853ccc8b3965c16f9a8
Weldon Thallysson <thallyssonprogramerr14anos@gmail.com>
2022-11-26 16:50:02 -0300

    Adicionando components ao modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PensamentosComponent } from './components/pensamentos/pensamentos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RodapeComponent,
+    HeaderComponent,
+    PensamentosComponent
   ],
   imports: [
     BrowserModule
