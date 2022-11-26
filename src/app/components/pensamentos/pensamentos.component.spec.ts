cd00123b42cc1b1c1669be8b0e16afbbc2862dbe
Weldon Thallysson <thallyssonprogramerr14anos@gmail.com>
2022-11-26 16:47:10 -0300

    criação do component pensamento

---
+++ src/app/components/pensamentos/pensamentos.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PensamentosComponent } from './pensamentos.component';
+
+describe('PensamentosComponent', () => {
+  let component: PensamentosComponent;
+  let fixture: ComponentFixture<PensamentosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PensamentosComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PensamentosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
